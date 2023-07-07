960aa0a6cc6298525272702fb4f2844a57f5bf31
Craig Gunson <craig.gunson@persuit.com>
2023-07-07 00:17:18 +0000

    fix: adjust url to work in our environment

--- src/server/ExpressApp.js
+++ src/server/ExpressApp.js
@@ -96,7 +96,7 @@ const setupExpressApp = (
   });
   
   app.use(express.static(path.join(__dirname, '../../dist')));
-  app.get('/', function (req, res) {
+  app.get('/sendgridmock', function (req, res) {
     res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
   });
   
